fc89a36dded05d3d18b3ba42b470218fb7cc8b97
irori <irorin@gmail.com>
2011-10-10 21:30:34 +0900

    Add goto macro.

--- proc.scm
+++ proc.scm
@@ -13,6 +13,8 @@
 
 (defmacro initial-pc report-the-current-state)
 
+(defmacro goto (cons))
+
 (define procedures (make-vector (lookup-enum 'max-procedure)))
 
 (define (define-proc name body)
@@ -21,13 +23,13 @@
 ; 76 Get user input; goto try_move if motion is requested
 (define-proc 'get-user-input
   '(lambda (world)
-     ((cons) cycle-label world)))
+     (goto cycle-label world)))
 
 ; 76 cycle:
 (define-proc 'cycle-label
   '(lambda (world)
      (let ((words listen))
-       ((cons) look-at-word1 (set-word12 world (lambda (_) words))))))
+       (goto look-at-word1 (set-word12 world (lambda (_) words))))))
 
 ; 78 Look at word1 and exit to the right place if it completes a command
 (define-proc 'look-at-word1
@@ -37,18 +39,18 @@
         (if (word? word1)
             (if (message-word? word1)
                 (begin ((nth (word-meaning word1) (message world)) #\newline I)
-                       ((cons) cycle-label world))
+                       (goto cycle-label world))
                 (print$ "wtf??"
-                        ((cons) quit world)))
+                        (goto quit world)))
             (print$ "I don't know that word.\n"  ; TODO: choose msg randomly
-                    ((cons) cycle-label world)))))))
+                    (goto cycle-label world)))))))
 
 ; 86 Report the current state
 (define-proc 'report-the-current-state
   '(lambda (world)
      (begin
        ((nth (location world) (long-desc world)) #\newline I)
-       ((cons) cycle-label world))))
+       (goto cycle-label world))))
 
 (define-proc 'quit
   '(lambda (world)
