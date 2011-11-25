9b1f34119813534fb99e6e59c36134a17e321f85
irori <irorin@gmail.com>
2011-11-26 00:21:44 +0900

    print uses print$ instead of string.

--- lib.scm
+++ lib.scm
@@ -80,12 +80,12 @@
 	 (cs (string->list (substring s 0 (- len 1)))))
     `(delay ,(fold list lastc cs))))
 
-(defmacro (print s)
-  ((string s) I))
-
 (defsyntax (print$ s e)
   ; (print$ "abc" e) => (#\c (#\b (#\a e)))
   (fold list e (string->list s)))
+
+(defmacro (print s)
+  (print$ s I))
 
 ;; boolean functions
 (defmacro not
